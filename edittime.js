cae497a1ad47cb121784fbf11b57affa34531bfe
armaldio <armaldio@gmail.com>
2014-11-13 22:50:39 +0100

    First release 3 themes - Loading editor - Change theme

---
+++ edittime.js
@@ -0,0 +1,224 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Code box",
+		"id":			"codebox",
+		"version":		"1.0",
+		"description":	"An input box with some code features",
+		"author":		"Armaldio",
+		"help url":		"http://www.scirra.com/manual/117/textbox",
+		"category":		"Form controls",
+		"type":			"world",			// appears in layout
+		"rotatable":	false,
+		"flags":		pf_position_aces | pf_size_aces,
+		"dependency":   "ace.js;theme-monokai.js;theme-chaos.js;theme-terminal.js;mode-javascript.js;worker-javascript.js;mode-c_cpp.js;mode-csharp.js;mode-vbscript.js;mode-css.js;mode-html.js;mode-php.js;mode-plain_text.js;mode-snippets.js;mode-sql.js"
+	};
+};
+
+////////////////////////////////////////
+// Parameter types:
+// AddNumberParam(label, description [, initial_string = "0"])			// a number
+// AddStringParam(label, description [, initial_string = "\"\""])		// a string
+// AddAnyTypeParam(label, description [, initial_string = "0"])			// accepts either a number or string
+// AddCmpParam(label, description)										// combo with equal, not equal, less, etc.
+// AddComboParamOption(text)											// (repeat before "AddComboParam" to add combo items)
+// AddComboParam(label, description [, initial_selection = 0])			// a dropdown list parameter
+// AddObjectParam(label, description)									// a button to click and pick an object type
+// AddLayerParam(label, description)									// accepts either a layer number or name (string)
+// AddLayoutParam(label, description)									// a dropdown list with all project layouts
+// AddKeybParam(label, description)										// a button to click and press a key (returns a VK)
+// AddAnimationParam(label, description)								// a string intended to specify an animation name
+// AddAudioFileParam(label, description)								// a dropdown list with all imported project audio files
+
+////////////////////////////////////////
+// Conditions
+
+// AddCondition(id,					// any positive integer to uniquely identify this condition
+//				flags,				// (see docs) cf_none, cf_trigger, cf_fake_trigger, cf_static, cf_not_invertible,
+//									// cf_deprecated, cf_incompatible_with_triggers, cf_looping
+//				list_name,			// appears in event wizard list
+//				category,			// category in event wizard list
+//				display_str,		// as appears in event sheet - use {0}, {1} for parameters and also <b></b>, <i></i>
+//				description,		// appears in event wizard dialog when selected
+//				script_name);		// corresponding runtime function name
+				
+AddCondition(0, cf_trigger, "On text changed", "code box", "On text changed", "Triggered when the text in the code box changes.", "OnTextChanged");
+
+////////////////////////////////////////
+// Actions
+
+// AddAction(id,				// any positive integer to uniquely identify this action
+//			 flags,				// (see docs) af_none, af_deprecated
+//			 list_name,			// appears in event wizard list
+//			 category,			// category in event wizard list
+//			 display_str,		// as appears in event sheet - use {0}, {1} for parameters and also <b></b>, <i></i>
+//			 description,		// appears in event wizard dialog when selected
+//			 script_name);		// corresponding runtime function name
+
+AddStringParam("Text", "The text to set in the code box.");
+AddAction(0, af_none, "Set text", "code box", "Set text to {0}", "Set the code box's text.", "SetText");
+
+AddStringParam("Tooltip", "The tooltip to set on the code box.");
+AddAction(2, af_none, "Set tooltip", "code box", "Set tooltip to {0}", "Set the code box's tooltip.", "SetTooltip");
+
+AddComboParamOption("Invisible");
+AddComboParamOption("Visible");
+AddComboParam("Visibility", "Choose whether to hide or show the code box.");
+AddAction(3, af_none, "Set visible", "Appearance", "Set <b>{0}</b>", "Hide or show the code box.", "SetVisible");
+
+AddComboParamOption("Disabled");
+AddComboParamOption("Enabled");
+AddComboParam("Mode", "Choose whether to enable or disable the code box.");
+AddAction(4, af_none, "Set enabled", "code box", "Set <b>{0}</b>", "Disable or enable the code box.", "SetEnabled");
+
+AddComboParamOption("Read-only");
+AddComboParamOption("Not read-only");
+AddComboParam("Mode", "Choose whether to enable or disable read-only mode.");
+AddAction(5, af_none, "Set read-only", "code box", "Set <b>{0}</b>", "Turn read-only on or off.", "SetReadOnly");
+
+AddAction(6, af_none, "Set focused", "code box", "Set focused", "Set the input focus to the code box.", "SetFocus");
+
+AddAction(8, af_none, "Set unfocused", "code box", "Set unfocused", "Remove the input focus from the code box.", "SetBlur");
+
+AddAction(9, af_none, "Scroll to bottom", "code box", "Scroll to bottom", "Scroll to the bottom of the code box", "ScrollToBottom");
+
+//Change editor theme
+AddComboParamOption("Monokai");
+AddComboParamOption("Chaos");
+AddComboParamOption("Terminal");
+AddComboParam("Theme", "Choose a theme : ");
+AddAction(10, af_none, "Set editor theme", "codebox", "Set editor theme to {0}", "Set the theme of the editor", "SetEditorTheme");
+
+//Change editor language
+AddComboParamOption("C++");
+AddComboParamOption("C#");
+AddComboParam("Language", "Choose a language : ");
+AddAction(11, af_none, "Set editor language", "codebox", "Set editor language to {0}", "Set the language of the editor", "SetEditorLang");
+
+AddAction(12, af_none, "Insert text at cursor", "codebox", "Insert text at cursor", "Insert text at cursor", "InsertTextAtCursor");
+
+AddNumberParam("Line", "The line number where to go");
+AddAction(13, af_none, "Go to a line", "codebox", "Go to line {0}", "Go to a line", "GoToALine");
+
+AddNumberParam("Size", "The size of the font");
+AddAction(14, af_none, "Set font size", "codebox", "Set font size", "Set font size", "setfontSize");
+
+AddAction(15, af_none, "Load editor", "codebox", "Load editor", "Load editor", "LoadEditor");
+////////////////////////////////////////
+// Expressions
+
+// AddExpression(id,			// any positive integer to uniquely identify this expression
+//				 flags,			// (see docs) ef_none, ef_deprecated, ef_return_number, ef_return_string,
+//								// ef_return_any, ef_variadic_parameters (one return flag must be specified)
+//				 list_name,		// currently ignored, but set as if appeared in event wizard
+//				 category,		// category in expressions panel
+//				 exp_name,		// the expression name after the dot, e.g. "foo" for "myobject.foo" - also the runtime function name
+//				 description);	// description in expressions panel
+
+AddExpression(0, ef_return_string, "Get text", "code box", "Text", "Get the code box's text.");
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	new cr.Property(ept_text,	"Text",					"",			"The initial text for the code box."),
+	new cr.Property(ept_text,	"Tooltip",				"",			"Display this text when hovering the mouse over the control."),
+	new cr.Property(ept_combo,	"Initial visibility",	"Visible",	"Choose whether the code box is visible on startup.", "Invisible|Visible"),
+	new cr.Property(ept_combo,	"Enabled",				"Yes",		"Choose whether the code box is enabled or disabled on startup.", "No|Yes"),
+	new cr.Property(ept_combo,	"Read-only",			"No",		"Choose whether the code box is read-only on startup.", "No|Yes"),
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+		
+	// Plugin-specific variables
+	this.just_inserted = false;
+	this.font = null;
+}
+
+IDEInstance.prototype.OnCreate = function()
+{
+	this.instance.SetHotspot(new cr.vector2(0, 0));
+}
+
+IDEInstance.prototype.OnInserted = function()
+{
+	this.instance.SetSize(new cr.vector2(150, 22));
+}
+
+IDEInstance.prototype.OnDoubleClicked = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+
+IDEInstance.prototype.OnRendererInit = function(renderer)
+{
+}
+	
+// Called to draw self in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+	if (!this.font)
+		this.font = renderer.CreateFont("Arial", 14, false, false);
+		
+	renderer.SetTexture(null);
+	var quad = this.instance.GetBoundingQuad();
+	renderer.Fill(quad, this.properties["Enabled"] === "Yes" ? cr.RGB(255, 255, 255) : cr.RGB(224, 224, 224));
+	renderer.Outline(quad, cr.RGB(0, 0, 0));
+	
+	cr.quad.prototype.offset.call(quad, 4, 2);
+	
+	if (this.properties["Text"].length)
+	{
+		this.font.DrawText(this.properties["Text"],
+							quad,
+							cr.RGB(0, 0, 0),
+							ha_left);
+	}
+	else
+	{
+		this.font.DrawText(this.properties["Placeholder"],
+							quad,
+							cr.RGB(128, 128, 128),
+							ha_left);
+	}
+}
+
+IDEInstance.prototype.OnRendererReleased = function(renderer)
+{
+	this.font = null;
+}
---
+++ runtime.js
@@ -0,0 +1,361 @@
+// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.codebox = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	/////////////////////////////////////
+	var pluginProto = cr.plugins_.codebox.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	// called on startup for each object type
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	// called whenever an instance is created
+	instanceProto.onCreate = function()
+	{
+		// Not supported in DC
+		if (this.runtime.isDomFree)
+		{
+			cr.logexport("[Construct 2] Textbox plugin not supported on this platform - the object will not be created");
+			return;
+		}
+		
+		//Creating editor
+		this.elem = document.createElement("div");
+		this.elem.setAttribute("id", "editor");
+
+		jQuery(this.elem).appendTo(this.runtime.canvasdiv ? this.runtime.canvasdiv : "body")
+			
+/*	
+		var onchangetrigger = (function (self) {
+			return function() {
+				self.runtime.trigger(cr.plugins_.TextBox.prototype.cnds.OnTextChanged, self);
+			};
+		})(this);
+
+		
+		this.elem["oninput"] = onchangetrigger;
+*/		
+		
+		this.lastLeft = 0;
+		this.lastTop = 0;
+		this.lastRight = 0;
+		this.lastBottom = 0;
+		this.lastWinWidth = 0;
+		this.lastWinHeight = 0;
+			
+		this.updatePosition(true);
+		
+		this.runtime.tickMe(this);
+	};
+	
+	instanceProto.saveToJSON = function ()
+	{
+		return {
+			"text": this.elem.value,
+			"placeholder": this.elem.placeholder,
+			"tooltip": this.elem.title,
+			"disabled": !!this.elem.disabled,
+			"readonly": !!this.elem.readOnly
+		};
+	};
+	
+	instanceProto.loadFromJSON = function (o)
+	{
+		this.elem.value = o["text"];
+		this.elem.placeholder = o["placeholder"];
+		this.elem.title = o["tooltip"];
+		this.elem.disabled = o["disabled"];
+		this.elem.readOnly = o["readonly"];
+	};
+	
+	instanceProto.onDestroy = function ()
+	{
+		if (this.runtime.isDomFree)
+				return;
+		
+		jQuery(this.elem).remove();
+		this.elem = null;
+	};
+	
+	instanceProto.tick = function ()
+	{
+		this.updatePosition();
+	};
+	
+	instanceProto.updatePosition = function (first)
+	{
+		if (this.runtime.isDomFree)
+			return;
+		
+		var left = this.layer.layerToCanvas(this.x, this.y, true);
+		var top = this.layer.layerToCanvas(this.x, this.y, false);
+		var right = this.layer.layerToCanvas(this.x + this.width, this.y + this.height, true);
+		var bottom = this.layer.layerToCanvas(this.x + this.width, this.y + this.height, false);
+		
+		// Is entirely offscreen or invisible: hide
+		if (!this.visible || !this.layer.visible || right <= 0 || bottom <= 0 || left >= this.runtime.width || top >= this.runtime.height)
+		{
+			if (!this.element_hidden)
+				jQuery(this.elem).hide();
+				
+			this.element_hidden = true;
+			return;
+		}
+		
+		// Truncate to canvas size
+		if (left < 1)
+			left = 1;
+		if (top < 1)
+			top = 1;
+		if (right >= this.runtime.width)
+			right = this.runtime.width - 1;
+		if (bottom >= this.runtime.height)
+			bottom = this.runtime.height - 1;
+		
+		var curWinWidth = window.innerWidth;
+		var curWinHeight = window.innerHeight;
+			
+		// Avoid redundant updates
+		if (!first && this.lastLeft === left && this.lastTop === top && this.lastRight === right && this.lastBottom === bottom && this.lastWinWidth === curWinWidth && this.lastWinHeight === curWinHeight)
+		{
+			if (this.element_hidden)
+			{
+				jQuery(this.elem).show();
+				this.element_hidden = false;
+			}
+			
+			return;
+		}
+			
+		this.lastLeft = left;
+		this.lastTop = top;
+		this.lastRight = right;
+		this.lastBottom = bottom;
+		this.lastWinWidth = curWinWidth;
+		this.lastWinHeight = curWinHeight;
+		
+		if (this.element_hidden)
+		{
+			jQuery(this.elem).show();
+			this.element_hidden = false;
+		}
+		
+		var offx = Math.round(left) + jQuery(this.runtime.canvas).offset().left;
+		var offy = Math.round(top) + jQuery(this.runtime.canvas).offset().top;
+		jQuery(this.elem).css("position", "absolute");
+		jQuery(this.elem).offset({left: offx, top: offy});
+		jQuery(this.elem).width(Math.round(right - left));
+		jQuery(this.elem).height(Math.round(bottom - top));
+		
+		if (this.autoFontSize)
+			jQuery(this.elem).css("font-size", ((this.layer.getScale(true) / this.runtime.devicePixelRatio) - 0.2) + "em");
+	};
+	
+	// only called if a layout object
+	instanceProto.draw = function(ctx)
+	{
+	};
+	
+	instanceProto.drawGL = function(glw)
+	{
+	};
+	
+	/**BEGIN-PREVIEWONLY**/
+	instanceProto.getDebuggerValues = function (propsections)
+	{
+		propsections.push({
+			"title": "codebox",
+			"properties": [
+				{"name": "Text", "value": this.elem.value},
+				{"name": "Placeholder", "value": this.elem["placeholder"]},
+				{"name": "Tooltip", "value": this.elem.title},
+				{"name": "Enabled", "value": !this.elem.disabled},
+				{"name": "Read-only", "value": !!this.elem["readOnly"]},
+			]
+		});
+	};
+	
+	instanceProto.onDebugValueEdited = function (header, name, value)
+	{
+		switch (name) {
+		case "Text":				this.elem.value = value;			break;
+		case "Placeholder":			this.elem["placeholder"] = value;	break;
+		case "Tooltip":				this.elem.title = value;			break;
+		case "Enabled":				this.elem.disabled = !value;		break;
+		case "Read-only":			this.elem["readOnly"] = value;		break;
+		}
+	};
+	/**END-PREVIEWONLY**/
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	
+	Cnds.prototype.OnTextChanged = function ()
+	{
+		return true;
+	};
+	
+	pluginProto.cnds = new Cnds();
+	
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	
+	Acts.prototype.SetText = function (text)
+	{
+		if (this.runtime.isDomFree)
+			return;
+		
+		this.elem.value = text;
+	};
+	
+	Acts.prototype.SetTooltip = function (text)
+	{
+		if (this.runtime.isDomFree)
+			return;
+		
+		this.elem.title = text;
+	};
+	
+	Acts.prototype.SetVisible = function (vis)
+	{
+		if (this.runtime.isDomFree)
+			return;
+		
+		this.visible = (vis !== 0);
+	};
+	
+	Acts.prototype.SetEnabled = function (en)
+	{
+		if (this.runtime.isDomFree)
+			return;
+		
+		this.elem.disabled = (en === 0);
+	};
+	
+	Acts.prototype.SetReadOnly = function (ro)
+	{
+		if (this.runtime.isDomFree)
+			return;
+		
+		this.elem.readOnly = (ro === 0);
+	};
+	
+	Acts.prototype.SetFocus = function ()
+	{
+		if (this.runtime.isDomFree)
+			return;
+		
+		this.elem.focus();
+	};
+	
+	Acts.prototype.SetBlur = function ()
+	{
+		if (this.runtime.isDomFree)
+			return;
+		
+		this.elem.blur();
+	};
+	
+	Acts.prototype.SetCSSStyle = function (p, v)
+	{
+		if (this.runtime.isDomFree)
+			return;
+			
+		jQuery(this.elem).css(p, v);
+	};
+	
+	Acts.prototype.ScrollToBottom = function ()
+	{
+		if (this.runtime.isDomFree)
+			return;
+		
+		this.elem.scrollTop = this.elem.scrollHeight;
+	};
+
+	//Armaldio Actions
+
+	Acts.prototype.SetEditorTheme = function (choice)
+	{
+		var editor = ace.edit('editor');
+		if (choice == 0) {
+			editor.setTheme('ace/theme/monokai');
+			console.log("Monokai loaded");
+		}else if (choice == 1) {
+			editor.setTheme('ace/theme/chaos');
+			console.log("Chaos loaded");
+		}else if (choice == 2) {
+			editor.setTheme('ace/theme/terminal');
+			console.log("Terminal loaded");
+		}
+	};
+
+	Acts.prototype.LoadEditor = function ()
+		{
+			var editor = ace.edit('editor');
+    		
+    		editor.getSession().setMode('ace/mode/javascript');
+    		editor.setValue('var dummyVar = "this is a dummy text";');
+		};
+	
+
+
+
+
+
+
+
+	
+	pluginProto.acts = new Acts();
+	
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	
+	Exps.prototype.Text = function (ret)
+	{
+		if (this.runtime.isDomFree)
+		{
+			ret.set_string("");
+			return;
+		}
+		
+		ret.set_string(this.elem.value);
+	};
+	
+	pluginProto.exps = new Exps();
+
+}());
